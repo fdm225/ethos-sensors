db869fc47d21495011a967319175f935addf7363
Personal Git <davidcmorrison@gmail.com>
2024-05-03 14:56:22 -0500

    - initial commit

---
+++ vfr24.lua
@@ -0,0 +1,74 @@
+function loadSched()
+    if not libSCHED then
+        -- Loadable code chunk is called immediately and returns libGUI
+        libSCHED = loadfile("sensorLib/libscheduler.lua")
+    end
+    return libSCHED()
+end
+
+----------------------------------------------------------------------------------------------------------------------
+local name = "VFR 2.4 min/current"
+local key = "vfr24"
+
+local function create()
+    local libservice = libservice or loadService()
+    g_mahRe2Service = g_mahRe2Service or libservice.new()
+
+    local libscheduler = libscheduler or loadSched()
+    g_scheduler = g_scheduler or libscheduler.new()
+    widget = {
+        vrf_current = 0,
+        service = g_mahRe2Service,
+    }
+    return widget
+end
+
+local function paint(widget)
+    lcd.font(FONT_XL)
+    local w, h = lcd.getWindowSize()
+    local displayString = "---/---"
+    if widget ~= nil then
+        displayString = math.floor(widget.service.vfr_24) .. "/" .. math.floor(widget.vrf_current)
+    end
+    local font_w, font_h = lcd.getTextSize(displayString)
+    --local x = (w - font_w)/2
+    local x = (w - font_w) / 2
+    local y = (h - font_h)/2
+
+    --lcd.color(lcd.RGB(0xF8, 0xB0, 0x38))
+    lcd.drawText(x, y, displayString)
+    lcd.invalidate()
+
+end
+
+local function wakeup(widget)
+    widget.service.reset_if_needed()
+    widget.vrf_current = system.getSource("VFR"):value()
+    if widget.service.vfr_24 < widget.vrf_current then
+        widget.service.vfr_24 = widget.vrf_current
+    end
+end
+
+local function configure(widget)
+    line = form.addLine("Reset Switch")
+    form.addSwitchField(line, form.getFieldSlots(line)[0], function()
+        return widget.service.resetSwitch
+    end, function(value)
+        widget.service.resetSwitch = value
+    end)
+end
+
+local function read(widget)
+    widget.service.resetSwitch = storage.read("resetSwitch")
+end
+
+local function write(widget)
+    storage.write("resetSwitch", widget.service.resetSwitch)
+end
+
+local function init()
+    system.registerWidget({ key = key, name = name, create = create, paint = paint, wakeup = wakeup,
+                            configure = configure, read = read, write = write, persistent = true })
+end
+
+return { init = init }
